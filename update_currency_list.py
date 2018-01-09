bbe1c0b65c5b329fc9ab49618c89e8b26863307f
Anthony <ajlusardi@gmail.com>
2018-01-09 16:26:35 -0500

    add script to update currency_list.json; currently manual

---
+++ update_currency_list.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+ticker_url = "https://api.coinmarketcap.com/v1/ticker/"
+response = requests.get(ticker_url, params={"limit" : 0})
+ticker = response.json()
+
+currency_dict = {}
+
+for item in ticker:
+    currency_dict[item["symbol"]] = item["name"]
+
+with open('currency_list.json', 'w') as file:
+    json.dump(currency_dict, file)
